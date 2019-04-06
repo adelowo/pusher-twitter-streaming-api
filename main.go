2f239dcc7822cb9dbc3b6dec5d674fb8a89b3e81
Lanre Adelowo <yo@lanre.wtf>
2019-04-06 16:29:01 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+	"github.com/joho/godotenv"
+)
+
+type cache struct {
+	counter map[string]int64
+	mu      sync.RWMutex
+}
+
+func (c *cache) Init(options ...string) {
+	for _, v := range options {
+		c.counter[v] = 0
+	}
+}
+
+func (c *cache) All() map[string]int64 {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return c.counter
+}
+
+func (c *cache) Incr(option string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.counter[option]++
+}
+
+func (c *cache) Count(option string) int64 {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	val, ok := c.counter[option]
+	if !ok {
+		return 0
+	}
+
+	return val
+}
+
+func main() {
+
+	options := flag.String("options", "Billy Ray Cyrus,Old Town Road,Word Bank", "What items to search for on Twitter")
+	httpPort := flag.Int("http.port", 1500, "What port to run HTTP on ?")
+
+	flag.Parse()
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("could not load .env file.. %v", err)
+	}
+
+	config := oauth1.NewConfig(os.Getenv("TWITTER_CONSUMER_KEY"), os.Getenv("TWITTER_CONSUMER_SECRET"))
+	token := oauth1.NewToken(os.Getenv("TWITTER_ACCESS_TOKEN"), os.Getenv("TWITTER_ACCESS_SECRET"))
+
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	client := twitter.NewClient(httpClient)
+
+	cache := &cache{
+		mu:      sync.RWMutex{},
+		counter: make(map[string]int64),
+	}
+
+	splittedOptions := strings.Split(*options, ",")
+
+	if n := len(splittedOptions); n < 2 {
+		log.Fatalf("There must be at least 2 options... %v ", splittedOptions)
+	} else if n > 3 {
+		log.Fatalf("There cannot be more than 3 options... %v", splittedOptions)
+	}
+
+	cache.Init(splittedOptions...)
+
+	go func() {
+
+		var t *template.Template
+		var once sync.Once
+
+		http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("."))))
+
+		http.Handle("/polls", http.HandlerFunc(poll(cache)))
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+			once.Do(func() {
+				tem, err := template.ParseFiles("index.html")
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				t = tem.Lookup("index.html")
+			})
+
+			t.Execute(w, nil)
+		})
+
+		http.ListenAndServe(fmt.Sprintf(":%d", *httpPort), nil)
+	}()
+
+	demux := twitter.NewSwitchDemux()
+	demux.Tweet = func(tweet *twitter.Tweet) {
+		for _, v := range splittedOptions {
+			if strings.Contains(tweet.Text, v) {
+				cache.Incr(v)
+			}
+		}
+	}
+
+	fmt.Println("Starting Stream...")
+
+	filterParams := &twitter.StreamFilterParams{
+		Track:         splittedOptions,
+		StallWarnings: twitter.Bool(true),
+	}
+
+	stream, err := client.Streams.Filter(filterParams)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go demux.HandleChan(stream.Messages)
+
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	log.Println(<-ch)
+
+	for _, v := range splittedOptions {
+		fmt.Println(cache.Count(v))
+	}
+
+	fmt.Println("Stopping Stream...")
+	stream.Stop()
+}
+
+func poll(cache *cache) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(cache.All())
+	}
+}
